bbc8dc6ba5e5222c240e39f8824a53f0cfe47c3c
amias-burrows <amias.burrows@gmail.com>
2022-02-21 17:55:38 +0000

    Changing 404 from h3 to p

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 	get_header('404');
 ?>
 <main id='homepage'>
-	<h3>Whoops!  We couldn't find what you were looking for.</br>Maybe you can find what you're looking for with a search.</h3>
+	<p>Whoops!  We couldn't find what you were looking for.</br>Maybe you can find what you're looking for with a search.</p>
 	<?php
 		get_search_form();
 	?>
