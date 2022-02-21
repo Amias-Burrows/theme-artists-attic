0349faf6d8d498a4ba9e87e8d077bf82a82dc1bc
amias-burrows <amias.burrows@gmail.com>
2022-02-21 18:04:12 +0000

    Centeralise 404 content

--- 404.php
+++ 404.php
@@ -2,10 +2,12 @@
 	get_header('404');
 ?>
 <main id='homepage'>
-	<p>Whoops!  We couldn't find what you were looking for.</br>Maybe you can find what you're looking for with a search.</p>
-	<?php
-		get_search_form();
-	?>
+	<div id='error'>
+		<p>Whoops!  We couldn't find what you were looking for.</br>Maybe you can find what you're looking for with a search.</p>
+		<?php
+			get_search_form();
+		?>
+	</div>
 </main>
 <?php
 	get_footer();
