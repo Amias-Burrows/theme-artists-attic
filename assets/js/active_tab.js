32ee9e59f7e836024a28c7ccc8310b781cb419a2
amias-burrows <amias.burrows@gmail.com>
2022-03-07 13:08:54 +0000

    Previous commit didn't work
    The code broke so I've rolled this one file back to it's initial commit
    state

--- assets/js/active_tab.js
+++ assets/js/active_tab.js
@@ -12,22 +12,7 @@ function find_active_tab(container) {
 			const exists = !!anchor.querySelector('ion-icon');
 			if (exists) {
 				const a_icon = anchor.querySelector('ion-icon');
-
-				if (a_icon.src != null) {	//If src is not null the icon is being taken from the website itself and therefore can have a unique title
-
-					if (a_icon.name == 'open') {
-
-						a_icon.name = 'close';
-						a_icon.src = a_icon.src.replace('.svg', '-outline.svg');
-					} else {
-
-						a_icon.name = 'open';
-						a_icon.src = a_icon.src.replace('-outline.svg', '.svg');
-					}
-				} else {	//If Icon does not use src attribute it is collecting the icon from the official website
-
-					a_icon.name += '-outline';
-				}
+				a_icon.name += '-outline';
 			}
 			return;
 		}
