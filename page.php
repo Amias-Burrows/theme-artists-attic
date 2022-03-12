54681be116e9ea40c15052cb4c74289b0ae405ac
amias-burrows <amias.burrows@gmail.com>
2022-03-12 12:15:13 +0000

    Made sass more inclusive of everything
    the #content was being used by header

--- page.php
+++ page.php
@@ -2,24 +2,20 @@
 	get_header('page');
 ?>
 <main>
-	<div id='content'>
-		<?php
-			
-			if (have_posts()) {
+<?php
+	
+	if (have_posts()) {
+	
+		while (have_posts()) {
 		
-				while (have_posts()) {
+			the_post();
+			the_content();
 		
-					the_post();
-					the_content();
-		
-				}
-		
-			}
-		
+		}
+	
+	}
 
-
-		?>
-	</div>
+?>
 </main>
 <?php
 	get_footer();
