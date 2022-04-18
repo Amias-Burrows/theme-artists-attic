60885193c546928a6159d93988a7b3991525d615
amias-burrows <amias.burrows@gmail.com>
2022-04-18 15:33:20 +0100

    Make footer work better on desktop
    I'm still waiting for the css to update so thats a thing ffs

--- footer.php
+++ footer.php
@@ -47,10 +47,10 @@
 				?>
 					<img id='logo' src='<?php echo $logo[0] ?>' alt='Site Logo'/>
 			</div>
-			<div id='smallprint'>
+			<section id='smallprint'>
 				<p>Charity Registration Number: 1198542</p>
 				<p>Address: C4D Carlinghow Mills, Batley, West Yorkshire, WF17 8LL</p>
-			</div>
+			</section>
 		</footer>
 	</body>
 	<?php
