80d6d1e69c84a3808b23ea8c8fb44eab1d0357c4
amias-burrows <amias.burrows@gmail.com>
2022-06-13 13:43:31 +0100

    Link to the menu nav section

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@
 		add_theme_support('title-tag');
 		add_theme_support('custom-logo');
 		add_theme_support('post-thumbnails');
-		add_theme_support('custom-header');
 		add_post_type_support('page', 'excerpt');
 	}
 
@@ -52,7 +51,7 @@
 		));
 		$wp_customize->add_section('artists_section', array(
 			'title' => __('Homepage Menu Images'),
-			'panel' => 'artists_panel'
+			'panel' => 'nav_menus'
 		));
 		$artists_menu = wp_get_nav_menu_items('homepage');
 		if ( ! $artists_menu ) {
