74947993a7c5d55451d20a0f7e16aba69014b2c5
amias-burrows <amias.burrows@gmail.com>
2022-07-02 13:58:22 +0100

    Test the bulk of code I just did
    Think I've finished but it's not been tested yet

--- front-page.php
+++ front-page.php
@@ -2,17 +2,51 @@
 	get_header();
 ?>
 <main class='ignore-width'>
+	<div class='artists-masonry'>
+		<div class='result'>
+			<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_1_image') ?>' alt='<?php echo get_theme_mod('artists_masonry_element_1_title') ?>'/>
+			<div id='content'>
+				<a href='<?php echo get_theme_mod('artists_masonry_element_1_link') ?>'><?php echo get_theme_mod('artists_masonry_element_1_title') ?></a>
+				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_1_description') ?></p>
+			</div>
+		</div>
+		<div class='result'>
+			<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_2_image') ?>' alt='<?php echo get_theme_mod('artists_masonry_element_2_title') ?>'/>
+			<div id='content'>
+				<a href='<?php echo get_theme_mod('artists_masonry_element_2_link') ?>'><?php echo get_theme_mod('artists_masonry_element_2_title') ?></a>
+				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_2_description') ?></p>
+			</div>
+		</div>
+		<div class='result'>
+			<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_3_image') ?>' alt='<?php echo get_theme_mod('artists_masonry_element_3_title') ?>'/>
+			<div id='content'>
+				<a href='<?php echo get_theme_mod('artists_masonry_element_3_link') ?>'><?php echo get_theme_mod('artists_masonry_element_3_title') ?></a>
+				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_3_description') ?></p>
+			</div>
+		</div>
+		<div class='result'>
+			<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_4_image') ?>' alt='<?php echo get_theme_mod('artists_masonry_element_4_title') ?>'/>
+			<div id='content'>
+				<a href='<?php echo get_theme_mod('artists_masonry_element_4_link') ?>'><?php echo get_theme_mod('artists_masonry_element_4_title') ?></a>
+				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_4_description') ?></p>
+			</div>
+		</div>
+		<div class='result'>
+			<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_5_image') ?>' alt='<?php echo get_theme_mod('artists_masonry_element_5_title') ?>'/>
+			<div id='content'>
+				<a href='<?php echo get_theme_mod('artists_masonry_element_5_link') ?>'><?php echo get_theme_mod('artists_masonry_element_5_title') ?></a>
+				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_5_description') ?></p>
+			</div>
+		</div>
+		<div class='result'>
+			<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_6_image') ?>' alt='<?php echo get_theme_mod('artists_masonry_element_6_title') ?>'/>
+			<div id='content'>
+				<a href='<?php echo get_theme_mod('artists_masonry_element_6_link') ?>'><?php echo get_theme_mod('artists_masonry_element_6_title') ?></a>
+				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_6_description') ?></p>
+			</div>
+		</div>
+	</div>
 	<?php
-		
-		wp_nav_menu(
-			array(
-				'menu' => 'homepage',
-				'menu_class' => 'artists-masonry',
-				'container' => 'ul',
-				'theme_location' => 'homepage',
-				'walker' => new walker_homepage()
-			)
-		);
 
 		if (have_posts()) {
 
--- functions.php
+++ functions.php
@@ -62,12 +62,263 @@
 		};
 	}
 
+	function artists_masonry($wp_customize) {
+		var $options = get_pages();
+		$options += get_categories();
+
+			// Title section for the masonry setting section
+		$wp_customize->add_section('artists_masonry_section', array(	
+			'title' => __('Homepage Menu', 'artists_masonry')
+		));
+
+			// TITLE FOR FIRST ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_1_title', array(
+			'default' => 'Title'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_1_title', array(
+			'label' => 'First Block Title',
+			'settings' => 'artists_masonry_element_1_title',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// DESCRIPTION FOR FIRST ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_1_description', array(
+			'default' => 'Description'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_1_description', array(
+			'label' => 'First Block Description',
+			'settings' => 'artists_masonry_element_1_description',
+			'section' => 'artists_masonry_section'
+		)));
+		
+			// LINK FOR FIRST ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_1_link', array(
+			'default' => 'Select a page or category from the dropdown'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_1_link', array(
+			'label' => 'First Block Link',
+			'settings' => 'artists_masonry_element_1_link',
+			'section' => 'artists_masonry_section',
+			'type' => 'select',
+			'choices' => $options
+		)));
+
+			// IMAGE FOR FIRST ELEMENT
+		$wp_customize->add_setting('artists_masonry_elements_1_image');
+		$wp_customize->add_control(new WP_Cuztomize_Image_Control($wp_customize, 'artists_masonry_elements_1_image', array(
+			'label' => 'First Block Background Image',
+			'settings' => 'artists_masonry_element_1_image',
+			'section' => 'artists_masonry_section'
+		)));
+		
+			// TITLE FOR SECOND ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_2_title', array(
+			'default' => 'Title'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_2_title', array(
+			'label' => 'First Block Title',
+			'settings' => 'artists_masonry_element_2_title',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// DESCRIPTION FOR SECOND ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_2_description', array(
+			'default' => 'Description'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_2_description', array(
+			'label' => 'First Block Description',
+			'settings' => 'artists_masonry_element_2_description',
+			'section' => 'artists_masonry_section'
+		)));
+		
+			// LINK FOR SECOND ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_2_link', array(
+			'default' => 'Select a page or category from the dropdown'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_2_link', array(
+			'label' => 'First Block Link',
+			'settings' => 'artists_masonry_element_2_link',
+			'section' => 'artists_masonry_section',
+			'type' => 'select',
+			'choices' => $options
+		)));
+
+			// IMAGE FOR SECOND ELEMENT
+		$wp_customize->add_setting('artists_masonry_elements_2_image');
+		$wp_customize->add_control(new WP_Cuztomize_Image_Control($wp_customize, 'artists_masonry_elements_2_image', array(
+			'label' => 'First Block Background Image',
+			'settings' => 'artists_masonry_element_2_image',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// TITLE FOR THIRD ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_3_title', array(
+			'default' => 'Title'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_3_title', array(
+			'label' => 'First Block Title',
+			'settings' => 'artists_masonry_element_3_title',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// DESCRIPTION FOR THIRD ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_3_description', array(
+			'default' => 'Description'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_3_description', array(
+			'label' => 'First Block Description',
+			'settings' => 'artists_masonry_element_3_description',
+			'section' => 'artists_masonry_section'
+		)));
+		
+			// LINK FOR THIRD ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_3_link', array(
+			'default' => 'Select a page or category from the dropdown'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_3_link', array(
+			'label' => 'First Block Link',
+			'settings' => 'artists_masonry_element_3_link',
+			'section' => 'artists_masonry_section',
+			'type' => 'select',
+			'choices' => $options
+		)));
+
+			// IMAGE FOR THIRD ELEMENT
+		$wp_customize->add_setting('artists_masonry_elements_3_image');
+		$wp_customize->add_control(new WP_Cuztomize_Image_Control($wp_customize, 'artists_masonry_elements_3_image', array(
+			'label' => 'First Block Background Image',
+			'settings' => 'artists_masonry_element_3_image',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// TITLE FOR FOURTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_4_title', array(
+			'default' => 'Title'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_4_title', array(
+			'label' => 'First Block Title',
+			'settings' => 'artists_masonry_element_4_title',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// DESCRIPTION FOR FOURTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_4_description', array(
+			'default' => 'Description'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_4_description', array(
+			'label' => 'First Block Description',
+			'settings' => 'artists_masonry_element_4_description',
+			'section' => 'artists_masonry_section'
+		)));
+		
+			// LINK FOR FOURTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_4_link', array(
+			'default' => 'Select a page or category from the dropdown'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_4_link', array(
+			'label' => 'First Block Link',
+			'settings' => 'artists_masonry_element_4_link',
+			'section' => 'artists_masonry_section',
+			'type' => 'select',
+			'choices' => $options
+		)));
+
+			// IMAGE FOR FOURTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_elements_4_image');
+		$wp_customize->add_control(new WP_Cuztomize_Image_Control($wp_customize, 'artists_masonry_elements_4_image', array(
+			'label' => 'First Block Background Image',
+			'settings' => 'artists_masonry_element_4_image',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// TITLE FOR FIFTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_5_title', array(
+			'default' => 'Title'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_5_title', array(
+			'label' => 'First Block Title',
+			'settings' => 'artists_masonry_element_5_title',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// DESCRIPTION FOR FIFTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_5_description', array(
+			'default' => 'Description'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_5_description', array(
+			'label' => 'First Block Description',
+			'settings' => 'artists_masonry_element_5_description',
+			'section' => 'artists_masonry_section'
+		)));
+		
+			// LINK FOR FIFTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_5_link', array(
+			'default' => 'Select a page or category from the dropdown'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_5_link', array(
+			'label' => 'First Block Link',
+			'settings' => 'artists_masonry_element_5_link',
+			'section' => 'artists_masonry_section',
+			'type' => 'select',
+			'choices' => $options
+		)));
+
+			// IMAGE FOR FIFTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_5_image');
+		$wp_customize->add_control(new WP_Cuztomize_Image_Control($wp_customize, 'artists_masonry_elements_5_image', array(
+			'label' => 'First Block Background Image',
+			'settings' => 'artists_masonry_element_5_image',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// TITLE FOR SIXTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_6_title', array(
+			'default' => 'Title'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_6_title', array(
+			'label' => 'First Block Title',
+			'settings' => 'artists_masonry_element_6_title',
+			'section' => 'artists_masonry_section'
+		)));
+
+			// DESCRIPTION FOR SIXTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_6_description', array(
+			'default' => 'Description'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_6_description', array(
+			'label' => 'First Block Description',
+			'settings' => 'artists_masonry_element_6_description',
+			'section' => 'artists_masonry_section'
+		)));
+		
+			// LINK FOR SIXTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_6_link', array(
+			'default' => 'Select a page or category from the dropdown'
+		));
+		$wp_customize->add_control(new WP_Customize_Control($wp_customize, 'artists_masonry_element_6_link', array(
+			'label' => 'First Block Link',
+			'settings' => 'artists_masonry_element_6_link',
+			'section' => 'artists_masonry_section',
+			'type' => 'select',
+			'choices' => $options
+		)));
+
+			// IMAGE FOR SIXTH ELEMENT
+		$wp_customize->add_setting('artists_masonry_element_6_image');
+		$wp_customize->add_control(new WP_Cuztomize_Image_Control($wp_customize, 'artists_masonry_elements_6_image', array(
+			'label' => 'First Block Background Image',
+			'settings' => 'artists_masonry_element_6_image',
+			'section' => 'artists_masonry_section'
+		)));
+	}
+
 	add_action('after_setup_theme', 'artists_theme_support');
 	add_action('widgets_init', 'artists_widgets_init');
 	add_action('init', 'artists_menus');
 	add_action('wp_enqueue_scripts', 'artists_register_styles');
 	add_action('wp_enqueue_scripts', 'artists_register_scripts');
 	add_action('customize_register', 'artists_menu_images');
+	add_action('customize_register', 'artists_masonry');
 
 
 	require('inc/walker.php');
