57cec8b5c9cd51aadac8e76ad2f94beb0f0835d3
amias-burrows <amias.burrows@gmail.com>
2022-07-02 17:02:36 +0100

    Check something else

--- front-page.php
+++ front-page.php
@@ -5,11 +5,14 @@
 	<div class='artists-masonry'>
 		<div class='result'>
 			<?php
+/*
 			if (get_theme_mod('artists_masonry_element_1_image') != null) {
 				?><img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_1_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_1_title'); ?>'/><?php
 			} else {
 				?><img id='thumbnail' src='https://picsum.photos/1024/600?blur=3&random=1' alt='<?php echo get_theme_mod('artists_masonry_element_1_title'); ?>'/><?php
+ */
 			?>
+				<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_1_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_1_title'); ?>'/>
 			<div id='content'>
 				<a href='<?php echo get_theme_mod('artists_masonry_element_1_link'); ?>'><?php echo get_theme_mod('artists_masonry_element_1_title'); ?></a>
 				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_1_description'); ?></p>
@@ -17,11 +20,14 @@
 		</div>
 		<div class='result'>
 			<?php
+/*
 			if (get_theme_mod('artists_masonry_element_2_image') != null) {
 				?><img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_2_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_2_title'); ?>'/><?php
 			} else {
 				?><img id='thumbnail' src='https://picsum.photos/1024/600?blur=3&random=2' alt='<?php echo get_theme_mod('artists_masonry_element_2_title'); ?>'/><?php
+ */
 			?>
+				<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_2_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_2_title'); ?>'/>
 			<div id='content'>
 				<a href='<?php echo get_theme_mod('artists_masonry_element_2_link'); ?>'><?php echo get_theme_mod('artists_masonry_element_2_title'); ?></a>
 				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_2_description'); ?></p>
@@ -29,11 +35,14 @@
 		</div>
 		<div class='result'>
 			<?php
+/*
 			if (get_theme_mod('artists_masonry_element_3_image') != null) {
 				?><img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_3_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_3_title'); ?>'/><?php
 			} else {
 				?><img id='thumbnail' src='https://picsum.photos/1024/600?blur=3&random=3' alt='<?php echo get_theme_mod('artists_masonry_element_3_title'); ?>'/><?php
+ */
 			?>
+				<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_3_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_3_title'); ?>'/>
 			<div id='content'>
 				<a href='<?php echo get_theme_mod('artists_masonry_element_3_link'); ?>'><?php echo get_theme_mod('artists_masonry_element_3_title'); ?></a>
 				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_3_description'); ?></p>
@@ -41,11 +50,14 @@
 		</div>
 		<div class='result'>
 			<?php
+/*
 			if (get_theme_mod('artists_masonry_element_4_image') != null) {
 				?><img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_4_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_4_title'); ?>'/><?php
 			} else {
 				?><img id='thumbnail' src='https://picsum.photos/1024/600?blur=3&random=4' alt='<?php echo get_theme_mod('artists_masonry_element_4_title'); ?>'/><?php
+ */
 			?>
+				<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_4_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_4_title'); ?>'/>
 			<div id='content'>
 				<a href='<?php echo get_theme_mod('artists_masonry_element_4_link'); ?>'><?php echo get_theme_mod('artists_masonry_element_4_title'); ?></a>
 				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_4_description'); ?></p>
@@ -53,11 +65,14 @@
 		</div>
 		<div class='result'>
 			<?php
+/*
 			if (get_theme_mod('artists_masonry_element_5_image') != null) {
 				?><img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_5_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_5_title'); ?>'/><?php
 			} else {
 				?><img id='thumbnail' src='https://picsum.photos/1024/600?blur=3&random=5' alt='<?php echo get_theme_mod('artists_masonry_element_5_title'); ?>'/><?php
+ */
 			?>
+				<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_5_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_5_title'); ?>'/>
 			<div id='content'>
 				<a href='<?php echo get_theme_mod('artists_masonry_element_5_link'); ?>'><?php echo get_theme_mod('artists_masonry_element_5_title'); ?></a>
 				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_5_description'); ?></p>
@@ -65,11 +80,14 @@
 		</div>
 		<div class='result'>
 			<?php
+/*
 			if (get_theme_mod('artists_masonry_element_6_image') != null) {
 				?><img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_6_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_6_title'); ?>'/><?php
 			} else {
 				?><img id='thumbnail' src='://picsum.photos/1024/600?blur=3&random=6' alt='<?php echo get_theme_mod('artists_masonry_element_6_title'); ?>'/><?php
+ */
 			?>
+				<img id='thumbnail' src='<?php echo get_theme_mod('artists_masonry_element_6_image'); ?>' alt='<?php echo get_theme_mod('artists_masonry_element_6_title'); ?>'/>
 			<div id='content'>
 				<a href='<?php echo get_theme_mod('artists_masonry_element_6_link'); ?>'><?php echo get_theme_mod('artists_masonry_element_6_title'); ?></a>
 				<p id='excerpt'><?php echo get_theme_mod('artists_masonry_element_6_description'); ?></p>
